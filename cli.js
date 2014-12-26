cdbb73e349e1510f3723e9640bbbc3993e1a3e31
Taylor Brennan <taylor.j.brennan@gmail.com>
2014-12-26 15:20:36 -0800

    implement cli args and -c to copy to clipboard

--- cli.js
+++ cli.js
@@ -3,12 +3,16 @@
 'use strict';
 
 var gimmeSoftware = require('./stackflow.js');
+var argv = require('minimist')(process.argv.slice(2));
+var clipboard = require('copy-paste');
 
 var green = '\u001b[32m';
 var red   = '\u001b[31m';
 var reset = '\u001b[39m';
 
-var myAwesomeStack = gimmeSoftware(process.argv[2]);
+var input = typeof argv.c === 'string' ? argv.c : argv._[0];
+
+var myAwesomeStack = gimmeSoftware(input);
 
 if (!myAwesomeStack) {
   return console.error('No suitable software stacks found. Try again.');
@@ -16,3 +20,12 @@ if (!myAwesomeStack) {
 
 console.log(green + myAwesomeStack.name + reset +
   ': ' + myAwesomeStack.stack.join(', '));
+
+if (argv.c) {
+  clipboard.copy(myAwesomeStack.name + ': ' +
+    myAwesomeStack.stack.join(', '), function (error) {
+      if (error) console.log(error);
+      else console.log('Copied to clipboard!');
+    }
+  );
+}
